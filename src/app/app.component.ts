2dfd0f76f513e595f9847994861900ba0c4d1388
Ehlullah Karakurt <contact@kaffio.co>
2026-02-22 16:07:54 +0300

    feat: Implement the main application component to display users, handle selection, and show tasks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
@@ -9,7 +10,7 @@ import { TasksComponent } from './tasks/tasks.component';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
 })
 export class AppComponent {
   users = DUMMY_USERS;
