4268d4d5150239a62c9e99fa98f03c6167131fd4
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2026-03-15 13:41:23 +0300

    feat: refactor dashboard items to use DashboardItemComponent for better structure and reusability

--- cmp-deep-dive/src/app/app.component.ts
+++ cmp-deep-dive/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TraficComponent } from './dashboard/trafic/trafic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { TicketsComponent } from './dashboard/tickets/tickets.component';
     ServerStatusComponent,
     TraficComponent,
     TicketsComponent,
+    DashboardItemComponent
   ],
   templateUrl: './app.component.html',
 })
