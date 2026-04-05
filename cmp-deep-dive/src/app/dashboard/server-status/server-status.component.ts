b69711ab7e770dbd6cec4cd5cc64aef36db688ca
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2026-04-05 10:55:10 +0300

    feat: implement ServerStatusComponent with signal-based status tracking and dynamic UI updates

--- cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,11 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,21 +15,26 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-  private destroyRef = inject(DestroyRef);
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
+  private readonly destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    console.log('CONSTRUCTOR');
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+  }
 
   ngOnInit() {
     console.log('ON INIT');
     const interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
