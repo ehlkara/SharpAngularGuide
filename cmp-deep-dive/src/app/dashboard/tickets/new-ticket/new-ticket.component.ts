bde74ee3fb763991b8845921249eac6421cbeb55
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2026-04-05 10:32:58 +0300

    feat: implement NewTicketComponent with signal-based viewChild for form reset functionality

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,11 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  //@ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private readonly form =
+    viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, ticketText: string) {
     console.log(title, ticketText);
 
-    this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
