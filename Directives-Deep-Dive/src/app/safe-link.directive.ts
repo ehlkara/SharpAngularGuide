f3220750f310346be05b9bf11382f8258bfe895d
Ehlullah Karakurt <contact@kaffio.co>
2026-04-26 17:36:08 +0300

    feat: add SafeLinkDirective to intercept and confirm external link navigation

--- Directives-Deep-Dive/src/app/safe-link.directive.ts
+++ Directives-Deep-Dive/src/app/safe-link.directive.ts
@@ -3,9 +3,22 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePaqge($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePaqge(event: MouseEvent) {
+    const wantToLeave = window.confirm(
+      'Are you sure you want to leave this page?',
+    );
+    if (wantToLeave) {
+      return;
+    }
+    event.preventDefault();
   }
 }
