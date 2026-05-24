1d7b0de64d193937cc9da756186aebfe6270a802
Ehlullah Karakurt <contact@kaffio.co>
2026-05-24 17:39:40 +0300

    feat: add CounterComponent and InfoMessageComponent with debug logging for change detection demonstration

--- change-detection/src/app/info-message/info-message.component.ts
+++ change-detection/src/app/info-message/info-message.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class InfoMessageComponent {
   get debugOutput() {
     console.log('[InfoMessages] "debugOutput" binding re-evaluated.');
-    return Math.random();
+    return 'InfoMessage Component Debug Output';
   }
 
   onLog() {
