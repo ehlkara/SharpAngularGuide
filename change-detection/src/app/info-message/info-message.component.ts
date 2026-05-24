0e6c567306aed4000a3a10398d46bf6fa4137321
Ehlullah Karakurt <contact@kaffio.co>
2026-05-24 17:34:10 +0300

    feat: add InfoMessageComponent with debug output and click logging

--- change-detection/src/app/info-message/info-message.component.ts
+++ change-detection/src/app/info-message/info-message.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class InfoMessageComponent {
   get debugOutput() {
     console.log('[InfoMessages] "debugOutput" binding re-evaluated.');
-    return 'InfoMessage Component Debug Output';
+    return Math.random();
   }
 
   onLog() {
