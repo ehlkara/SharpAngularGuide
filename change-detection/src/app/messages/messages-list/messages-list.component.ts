3b8368edbae5662aa5821b2ef90ebacbe54658c8
Ehlullah Karakurt <contact@kaffio.co>
2026-06-04 13:53:02 +0300

    feat: initialize change detection deep dive project with experimental zoneless detection and signal-based components

--- change-detection/src/app/messages/messages-list/messages-list.component.ts
+++ change-detection/src/app/messages/messages-list/messages-list.component.ts
@@ -5,15 +5,13 @@ import { MessagesService } from '../messages.service';
 @Component({
   selector: 'app-messages-list',
   standalone: true,
-  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-
-  messages$ = this.messagesService.messages$;
+  messages = this.messagesService.allMessages;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
