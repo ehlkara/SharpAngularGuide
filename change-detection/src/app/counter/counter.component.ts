190c860b9fb0ffe3596f09dea360ba5b7ee806a9
Ehlullah Karakurt <contact@kaffio.co>
2026-06-04 13:55:30 +0300

    feat: implement CounterComponent with manual increment and decrement logic

--- change-detection/src/app/counter/counter.component.ts
+++ change-detection/src/app/counter/counter.component.ts
@@ -11,7 +11,7 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
-  count = signal(0);
+  count = 0;
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
@@ -20,7 +20,7 @@ export class CounterComponent implements OnInit {
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.count.set(0);
+      this.count = 0;
     }, 4000);
 
     setTimeout(() => {
@@ -29,10 +29,12 @@ export class CounterComponent implements OnInit {
   }
 
   onDecrement() {
-    this.count.update((prevCount) => prevCount - 1);
+    //this.count.update((prevCount) => prevCount - 1);
+    this.count -= 1;
   }
 
   onIncrement() {
-    this.count.update((prevCount) => prevCount + 1);
+    //this.count.update((prevCount) => prevCount + 1);
+    this.count += 1;
   }
 }
