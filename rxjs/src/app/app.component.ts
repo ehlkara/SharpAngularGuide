5f15ce237bc21ab6c97527bc07e5174da174a228
Ehlullah Karakurt <contact@kaffio.co>
2026-06-21 15:56:53 +0300

    feat: implement click counter with RxJS observable and DestroyRef subscription cleanup in AppComponent

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,31 +9,35 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
   // interval = signal(0);
   // doubleInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
-  private message = signal('Hello!');
+  // private message = signal('Hello!');
 
   constructor() {
     // effect(() => {
     //   console.log('Clicked ', this.clickCount(), ' times.')
     // });
-    effect(() => {
-
-    })
   }
 
   ngOnInit(): void {
     // setInterval(() => {
     //   this.interval.set(this.interval() + 1);
     // }, 1000);
-    console.log(this.message());
-    const subscription = interval(1000).pipe(
-      map((val) => val * 10)
-    ).subscribe({
-      next: (val) => console.log(val),
-    });
+    // console.log(this.message());
+    // const subscription = interval(1000).pipe(
+    //   map((val) => val * 10)
+    // ).subscribe({
+    //   next: (val) => console.log(val),
+    // });
 
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+    const subscription = this.clickCount$.subscribe({
+      next: (value) => console.log('Clicked', value, 'times')
+    });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
