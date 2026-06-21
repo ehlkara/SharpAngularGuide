d9f64a468a5a9cad3728e97d5c5b1335678994ce
Ehlullah Karakurt <contact@kaffio.co>
2026-06-21 16:17:01 +0300

    feat: implement RxJS interval and signal integration in app component

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +14,20 @@ export class AppComponent implements OnInit {
   intervalSignal = toSignal(this.interval$, { initialValue: 0 });
   // interval = signal(0);
   // doubleInterval = computed(() => this.interval() * 2);
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      // subscriber.error();
+      if (timesExecuted > 5) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value...')
+      subscriber.next({ message: 'New Value' });
+      timesExecuted++;
+    }, 2000);
+  });
   private destroyRef = inject(DestroyRef);
   // private message = signal('Hello!');
 
@@ -37,6 +51,11 @@ export class AppComponent implements OnInit {
     // this.destroyRef.onDestroy(() => {
     //   subscription.unsubscribe();
     // });
+    this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('Interval completed'),
+      error: (error) => console.log('Error', error)
+    });
     const subscription = this.clickCount$.subscribe({
       next: (value) => console.log('Clicked', value, 'times')
     });
